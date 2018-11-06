a6205941a2da34223b35ceb829a76c1707a93597
bstelly <bstelly1999@gmail.com>
2018-11-06 17:55:13 -0600

    Worked on Calculator

--- Assets/Scripts/Physics/ProjectileMovement.cs
+++ Assets/Scripts/Physics/ProjectileMovement.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Assertions.Comparers;
 
 
 namespace Assets.Scripts.Physics
@@ -13,24 +14,76 @@ namespace Assets.Scripts.Physics
     public class ProjectileMovement : ScriptableObject
     {
         public Vector2 InitialPosition;
+        [HideInInspector]
         public Vector2 FinalPosition;
         public Vector2 InitialVelocity;
-        public Vector2 FinalVelocity;
-        private float gravity;           //g
+        private Vector2 FinalVelocity;
+        public readonly float gravity = 9.81f;  //g
         public float time;              //t
 
-        public void CalcAngleOfLaunch()
+
+        public float CalcAngleOfLaunch()
         {
-            //horizontal velocity
-            float theta = Mathf.Atan(InitialVelocity.y / InitialVelocity.x);
+            float theta = 0;
+            if (InitialVelocity.x > 0 && InitialVelocity.y > 0)
+            {
+                theta = Mathf.Atan(InitialVelocity.y / InitialVelocity.x);
+            }
+            //else if (InitialVelocity.x > 0 && InitialVelocity.y <= 0)
+            //{
+            //    theta = Mathf.Acos(InitialVelocity.x / InitialVelocity)
+            //}
+            //else if (InitialVelocity.x <= 0 && InitialVelocity.y > 0)
+            //{
+
+            //}
+            return theta;
         }
 
-        public void CalcTimeOfFlight()
+        public Vector2 CalcFinalPosition()
         {
-            time = 2 * (InitialVelocity.y / gravity);
+            FinalPosition.x = CalcDistance() + InitialPosition.x;
+            return FinalPosition;
         }
 
-        public void CalcDistance()
+        public float CalcTimeOfFlight()
+        {
+            return time = 2 * (InitialVelocity.y / gravity);
+        }
+
+        public float CalcDistance()
+        {
+            float distance = 0;
+            if (InitialVelocity.y != 0)
+            {
+                distance = (2 * InitialVelocity.x * InitialVelocity.y) / gravity;
+            }
+            else
+            {
+                distance = InitialVelocity.x * time;
+            }
+            return distance;
+        }
+
+        public void CalcInitialVelocity()
+        {
+            InitialVelocity.x = FinalVelocity.x - (gravity * time);
+            InitialVelocity.y = FinalVelocity.y - (gravity * time);
+        }
+
+        public void CalcFinalVelocity()
+        {
+            FinalVelocity.x = InitialVelocity.x + (gravity * time);
+            FinalVelocity.y = InitialVelocity.y + (gravity * time);
+        }
+
+        public void CalcHorizontalVelocity()
+        {
+            float distance = CalcDistance();
+            InitialVelocity.x = distance / time;
+        }
+
+        public void CalcVerticalVelocity()
         {
 
         }
@@ -46,7 +99,7 @@ namespace Assets.Scripts.Physics
             //}
             //else
             //{
-                var result = (InitialVelocity.y * InitialVelocity.y) / (2 * 9.81f);
+                var result = InitialPosition.y + ((InitialVelocity.y * InitialVelocity.y) / (2 * gravity));
                 return result;
             //}
 
@@ -62,26 +115,17 @@ namespace Assets.Scripts.Physics
 
             ProjectileMovement myProjectileMovement = (ProjectileMovement) target;
 
-            float maxHeight = 0;
-
-
             GUIStyle boxStyle = new GUIStyle(GUI.skin.box);
             boxStyle.normal.textColor = Color.white;
 
-            if(GUILayout.Button("find angle"))
-            {
-                myProjectileMovement.CalcAngleOfLaunch();
-            }
+            GUILayout.Box("Final Position = " + myProjectileMovement.CalcFinalPosition(), boxStyle);
+            GUILayout.Box("Angle of Launch = " + myProjectileMovement.CalcAngleOfLaunch() + " rad" + " / " + 
+                ((myProjectileMovement.CalcAngleOfLaunch() * 180) / Mathf.PI) + " deg", boxStyle);
+            GUILayout.Box("Time of Flight = " + myProjectileMovement.CalcTimeOfFlight() + " seconds", boxStyle);
+            GUILayout.Box("Distance = " + myProjectileMovement.CalcDistance() + " meters", boxStyle);
+            GUILayout.Box("Maximum Height = " + myProjectileMovement.CalcMaximumHeight() + " meters", boxStyle);
 
-            GUILayout.Box("Maximum Height = " + myProjectileMovement.CalcMaximumHeight() + " meters" + "\n", boxStyle);
 
         }
     }
 }
-
-
-
-// Vx = Vo cos(theta) 19.82
-// Vy = Vo sin(theta) - g * t 
-// x = Vo cos(theta) * t
-// y = Vo sin(theta) t - (1/2) g * t2
