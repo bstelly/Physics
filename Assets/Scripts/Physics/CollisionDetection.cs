433589d80f6efbe4ea21231e921efba66ec1e310
bstelly <bstelly1999@gmail.com>
2018-11-13 17:55:21 -0600

    Collision detection

---
+++ Assets/Scripts/Physics/CollisionDetection.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Physics
+{
+
+    public class CollisionDetection : MonoBehaviour
+    {
+
+        [NonSerialized] public List<float> xValues;
+
+        [NonSerialized] public List<float> yValues;
+
+        public List<CollisionVolume2D> colliders;
+
+        public Transform collisionVolumes;
+
+        void Start()
+        {
+            foreach (Transform child in collisionVolumes)
+            {
+                colliders.Add(child.GetComponent<CollisionVolume2D>());
+            }
+        }
+
+        void Update()
+        {
+            for (int i = 0; i < colliders.Count; i++)
+            {
+                for (int j = 0; j < colliders.Count; j++)
+                {
+                    if (colliders[i] != colliders[j])
+                    {
+                        if (colliders[i].min.x < colliders[j].max.x &&
+                            colliders[i].max.x > colliders[j].min.x &&
+                            colliders[i].min.y < colliders[j].max.y &&
+                            colliders[i].max.y > colliders[j].min.y)
+                        {
+                            Debug.Log("collision detected");
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
+
---
+++ Assets/Scripts/Physics/CollisionVolume2D.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Assertions.Comparers;
+
+
+namespace Assets.Scripts.Physics
+{
+    public class CollisionVolume2D : MonoBehaviour
+    {
+        public Vector2 min;
+        public Vector2 max;
+
+        public bool isColliding;
+
+        void Start()
+        {
+            min = new Vector2(transform.position.x - (transform.localScale.x / 2),
+                transform.position.y - (transform.localScale.y / 2));
+            max = new Vector2(transform.position.x + (transform.localScale.x / 2),
+                transform.position.y + (transform.localScale.y / 2));
+        }
+
+        void Update()
+        {
+            min = new Vector2(transform.position.x - (transform.localScale.x / 2),
+                transform.position.y - (transform.localScale.y / 2));
+            max = new Vector2(transform.position.x + (transform.localScale.x / 2),
+                transform.position.y + (transform.localScale.y / 2));
+        }
+    }
+}
