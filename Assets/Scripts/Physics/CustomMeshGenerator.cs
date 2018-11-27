c9deb7f505ac10ecd672d4b767981ab68f362c4f
bstelly <bstelly1999@gmail.com>
2018-11-27 17:52:21 -0600

    cloth

---
+++ Assets/Scripts/Physics/CustomMeshGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Policy;
+using UnityEngine;
+
+public class CustomMeshGenerator {
+    List<Vector3> vertices = new List<Vector3>();
+    List<int> TrianglePoints = new List<int>();
+    List<Vector3> SurfaceNormals = new List<Vector3>();
+    List<Vector2> UVs = new List<Vector2>();
+    public MeshFilter InstanceMeshFilter;
+    public Mesh InstanceMesh;
+}
